5c18143de9695a9eeb453883bb833b2e1a995430
lachierigg <lachrigg@gmail.com>
2024-10-18 10:42:22 +1100

    Created the typing effect for my name and dev description on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,42 @@
+"use client";
+
+import { useEffect } from "react";
 import "./page.css";
 
 export default function Main() {
+  useEffect(() => {
+    const typeSpeed = 80;
+    let timeoutId: number | NodeJS.Timeout;
+
+    function type(id: string, text: string, callback?: () => void) {
+      let i = 0;
+      const typedElement = document.getElementById(id);
+      if (typedElement) {
+        typedElement.innerHTML = ""; // Ensure the element is empty
+        const typeChar = () => {
+          if (i < text.length) {
+            typedElement.innerHTML += text.charAt(i);
+            i++;
+            timeoutId = setTimeout(typeChar, typeSpeed);
+          } else if (callback) {
+            callback();
+          }
+        };
+        typeChar();
+      }
+    }
+
+    type("name", "Lachie Rigg", () => {
+      type("desc", "Full-Stack Developer");
+    });
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
-    <div className="h-screen flex flex-col justify-center items-center">
-      <div className="text-8xl text-white custom-font">
-        Lachie Rigg
-      </div>
-      <div className="text-2xl text-white mt-16 font-bold">
-        Full-Stack Developer
-      </div>
+    <div className="h-screen flex flex-col justify-center items-center mb-16">
+      <div id="name" className="text-8xl text-white custom-font"></div>
+      <div id="desc" className="text-2xl text-white font-bold"></div>
     </div>
   );
 }
