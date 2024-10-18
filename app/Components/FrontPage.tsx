0939bb165ae488d1b7261946dd6774385df42a1b
lachierigg <lachrigg@gmail.com>
2024-10-18 20:14:26 +1100

    added navigation bar with social links

---
+++ app/Components/FrontPage.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import TypingEffect from "./Animations/TypingEffect";
+import Navbar from "./Navbar";
+import { Meteors } from "./Animations/MeteorEffect";
+import "../page.css";
+
+export default function FrontPage() {
+  const [showDesc, setShowDesc] = useState(false);
+  const [showButton, setShowButton] = useState(false);
+  const [showMeteors, setShowMeteors] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowDesc(true);
+    }, 1000);
+
+    const buttonTimer = setTimeout(() => {
+      setShowButton(true);
+    }, 1000);
+
+    const meteorTimer = setTimeout(() => {
+      setShowMeteors(true);
+    }, 1110);
+
+    return () => {
+      clearTimeout(timer);
+      clearTimeout(buttonTimer);
+      clearTimeout(meteorTimer);
+    };
+  }, []);
+
+  return (
+    <div className="bg-neutral-900 h-screen">
+      <Navbar />
+      {showMeteors && <Meteors number={20} />}
+      <div className="bg-neutral-950 flex justify-center flex-col items-center text-center w-[100%] h-[100%] border border-neutral-950 shadow-d shadow-white">
+        <div className="font-popsies text-8xl text-orange-200 fade-in">
+          Lachie Rigg
+        </div>
+        {!showDesc && <div className="mt-[68px] mb-[64px]"></div>}
+        {showDesc && (
+          <TypingEffect
+            id="desc"
+            text="Full Stack Web Developer"
+            className="font-roboto text-2xl mt-10 mb-16 text-orange-100"
+          />
+        )}
+        <button
+          type="button"
+          className={`font-roboto text-orange-100 hover:text-white border border-gray-800 hover:bg-neutral-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-10 py-2.5 text-center ${
+            showButton ? "fade-in" : "invisible"
+          }`}
+        >
+          Click Here To See My Work!
+        </button>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,59 +1,12 @@
+// app/page.tsx
 "use client";
 
-import { useState, useEffect } from "react";
-import TypingEffect from "./Components/Animations/TypingEffect";
-import { Meteors } from "./Components/Animations/MeteorEffect";
-import "./page.css";
+import FrontPage from "./Components/FrontPage";
 
 export default function Main() {
-  const [showDesc, setShowDesc] = useState(false);
-  const [showButton, setShowButton] = useState(false);
-  const [showMeteors, setShowMeteors] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowDesc(true);
-    }, 1000);
-
-    const buttonTimer = setTimeout(() => {
-      setShowButton(true);
-    }, 1000);
-
-    const meteorTimer = setTimeout(() => {
-      setShowMeteors(true);
-    }, 1010);
-
-    return () => {
-      clearTimeout(timer);
-      clearTimeout(buttonTimer);
-      clearTimeout(meteorTimer);
-    };
-  });
-
   return (
-    <div className="bg-neutral-900 h-screen flex flex-col justify-center items-center relative z-10">
-      {showMeteors && <Meteors number={15} />}
-      <div className="bg-neutral-900 flex justify-center flex-col items-center text-center w-[100%] h-[100%] border border-neutral-800 shadow-d shadow-white">
-        <div className="font-popsies text-8xl text-orange-100 fade-in">
-          Lachie Rigg
-        </div>
-        {!showDesc && <div className="mt-[68px] mb-[64px]"></div>}
-        {showDesc && (
-          <TypingEffect
-            id="desc"
-            text="Full-Stack Web Developer"
-            className="font-roboto text-2xl mt-10 mb-16 text-orange-100"
-          />
-        )}
-        <button
-          type="button"
-          className={`font-roboto text-orange-100 text-lg bg-neutral-800 border border-neutral hover:bg-neutral-950 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg px-10 py-2.5 text-center ${
-            showButton ? "fade-in" : "invisible"
-          }`}
-        >
-          Click Here To See My Work!
-        </button>
-      </div>
+    <div>
+      <FrontPage />
     </div>
   );
 }
