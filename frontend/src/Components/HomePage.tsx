a87c641ab722b1f2552a64cca32498b2ea98f295
Lachlan Rigg <111475731+lachierigg05@users.noreply.github.com>
2025-02-21 21:43:01 +1100

    Navbar starter design

---
+++ frontend/src/Components/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+export const HomePage: React.FC = () => {
+    return (
+        <div className="h-screen w-full flex justify-center">
+            <NavBar/>
+        </div>
+    )
+}
+
+export default HomePage; 
--- frontend/src/Components/NavBar.tsx
+++ frontend/src/Components/NavBar.tsx
@@ -1,8 +1,21 @@
-const NavBar = () => {
+import React from 'react';
+
+export const NavBar: React.FC = () => {
     return (
-        <>
-        </>
-    );
-};
+        <div className="h-28 w-[99%] flex m-2 bg-gray-950 border border-gray-800 shadow-2xl rounded shadow-blue-900">
+            <div className='flex h-full w-1/2 justify-start items-center'>
+                <h1 className='text-white m-20'>Lachie Rigg</h1>
+            </div>
+            <div className='w-1/2 mr-4 flex justify-end items-center'>
+                <li className='text-white m-5'>Home</li>
+                <li className='text-white m-5'>About</li>
+                <li className='text-white m-5'>Projects</li>
+                <li className='text-white m-5'>Contact</li>
+            </div>
+            
+        </div>
+        
+    )
+}
 
 export default NavBar;
