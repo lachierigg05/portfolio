aae78284006d835d62beea93c27794263c524496
Lachlan Rigg <111475731+lachierigg05@users.noreply.github.com>
2025-02-21 22:20:45 +1100

    Added custom font for name heading and futher styling to the navbar

--- frontend/src/Components/NavBar.tsx
+++ frontend/src/Components/NavBar.tsx
@@ -5,7 +5,7 @@ export const NavBar: React.FC = () => {
     return (
         <div className="h-28 w-[99%] flex m-2 bg-gray-950 border border-gray-800 shadow-2xl rounded shadow-blue-900">
             <div className='flex h-full w-1/2 justify-start items-center'>
-                <h1 className='text-white m-20'>Lachie Rigg</h1>
+                <h1 className='text-white ml-14 mt-4 font-popsies text-3xl'>Lachie Rigg</h1>
             </div>
             <div className='w-1/2 mr-4 flex justify-end items-center'>
                 <NavButton label='Home' onClick={() => console.log('Home')} />
--- frontend/src/Components/NavButton.tsx
+++ frontend/src/Components/NavButton.tsx
@@ -7,7 +7,7 @@ interface NavButtonProps {
 
 const NavButton: React.FC<NavButtonProps> = ({ label, onClick }) => {
     return (
-        <button onClick={onClick} className='group text-white m-5 h-14 w-22 bg-gray-950 hover:cursor-pointer rounded shadow-2xl relative'>
+        <button onClick={onClick} className='group text-white m-5 h-14 w-22 bg-gray-950 hover:cursor-pointer rounded shadow-2xl relative font-roboto'>
             {label}
             <span className="absolute bottom-2 left-[20%] w-0 h-[1px] bg-white transition-all duration-400 group-hover:w-[60%]"></span>
         </button>
