5a1655e427c7226f4a50fcc07e6094cf87a2705d
Lachie Rigg <lachrigg@gmail.com>
2025-02-22 20:53:55 +1100

    Navbar restructuring during mobile view, further polishing of animations

--- frontend/src/Components/NavBar.tsx
+++ frontend/src/Components/NavBar.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react'; 
 import NavButton from './NavButton';
+import { Menu, WindArrowDown } from 'lucide-react';
 
 export const NavBar: React.FC = () => {
     const [showNavbar, setShowNavBar] = useState(false);
+    const [isMobView, setMobView] = useState(false);
+    const [isMenuOpen, setMenuOpen] = useState(false); 
     
+    // Effect hook to handle navbar view when scrolling past 75 pixels
     useEffect(() => {
         const handleScroll = () => {
             const offset = window.scrollY; 
@@ -20,26 +24,44 @@ export const NavBar: React.FC = () => {
             window.removeEventListener('scroll', handleScroll);
         }
     })
-    
+
+    // Effect hook to restructure nav bar when width reaches mobile size 
+    useEffect(() => {
+        const checkScreenSize = () => {setMobView(window.innerWidth >= 650);};
+        checkScreenSize()
+        window.addEventListener("resize", checkScreenSize); 
+        return(() => {window.removeEventListener("resize", checkScreenSize)}); 
+    }, [])
     
     return (
-        <nav className={`fixed flex flex-row justify-center items-center t-0 h-30 w-full bg-gray-950 shadow-2xl shadow-blue-900 transition-transform duration-500 ${showNavbar ? 'translate-y-0' : '-translate-y-100'}`}>
+        <nav className={`fixed flex flex-row justify-center items-center t-0 h-30 w-full bg-gray-950 shadow-2xl shadow-blue-950 transition-transform duration-500 ${showNavbar ? 'translate-y-0' : '-translate-y-100'}`}>
             <div className="flex h-full w-1/2 ml-10 items-center mt-4">
                 <h1 className={'text-white font-popsies text-4xl'}>Lachie Rigg</h1>
             </div>
             <div className={`flex h-full w-1/2 justify-end mt-6`}>
-                <NavButton label={'Home'} onClick={function (): void {
-                    const element = document.getElementById('home');
-                    element?.scrollIntoView({behavior: 'smooth'});  
-                } }/>
-                <NavButton label={'Projects'} onClick={function (): void {
-                    const element = document.getElementById('projects');
-                    element?.scrollIntoView({behavior: 'smooth'});  
-                } }/>
-                <NavButton label={'Contact'} onClick={function (): void {
-                    const element = document.getElementById('contact');
-                    element?.scrollIntoView({behavior: 'smooth'});  
-                } }/>
+                { isMobView ? (
+                    // Desktop View 
+                    <div className='flex justify-end w-full h-full'>
+                        <NavButton label={'Home'} onClick={function (): void {
+                            const element = document.getElementById('home');
+                            element?.scrollIntoView({behavior: 'smooth'});  
+                        } }/>
+                        <NavButton label={'Projects'} onClick={function (): void {
+                            const element = document.getElementById('projects');
+                            element?.scrollIntoView({behavior: 'smooth'});  
+                        } }/>
+                        <NavButton label={'Contact'} onClick={function (): void {
+                            const element = document.getElementById('contact');
+                            element?.scrollIntoView({behavior: 'smooth'});  
+                        } }/>
+                    </div>
+                ) : (
+                    // Mobile View 
+                    <div className={`flex h-full w-full justify-end m-8`}>
+                        <Menu size={30} color="white" onClick={() => {setMenuOpen(true)}}/>
+                    </div>
+                )     
+            }    
             </div>
         </nav>
     )
--- frontend/src/Components/StaticNav.tsx
+++ frontend/src/Components/StaticNav.tsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react'; 
 import NavButton from './NavButton';
+import { Menu } from 'lucide-react';
 
 const StaticNav: React.FC = () => {
     const [inView, setInView] = useState(false);
+    const [isMobView, setMobView] = useState(false); 
+        const [isMenuOpen, setMenuOpen] = useState(false); 
+    
 
     useEffect(() => {
         setInView(true);
@@ -24,25 +28,44 @@ const StaticNav: React.FC = () => {
 
     }, []);
 
+        // Effect hook to restructure nav bar when width reaches mobile size 
+        useEffect(() => {
+            const checkScreenSize = () => {setMobView(window.innerWidth >= 650);};
+            checkScreenSize()
+            window.addEventListener("resize", checkScreenSize); 
+            return(() => {window.removeEventListener("resize", checkScreenSize)}); 
+        }, [])
+
     
     return (
         <nav className={`fixed flex justify-start h-30 w-full bg-gray-950 shadow-blue-500 transition-opacity duration-800 ${inView ? 'opacity-100' : 'opacity-0'}`}>
-            <div className="flex h-full w-1/2 ml-10 items-center">
+            <div className="flex h-full w-1/2 ml-10 items-center mt-4">
                 <h1 className={'text-white font-popsies text-4xl'}>Lachie Rigg</h1>
             </div>
-            <div className={`flex h-full w-1/2 justify-end`}>
-                <NavButton label={'Home'} onClick={function (): void {
-                    const element = document.getElementById('home');
-                    element?.scrollIntoView({behavior: 'smooth'});  
-                } }/>
-                <NavButton label={'Projects'} onClick={function (): void {
-                    const element = document.getElementById('projects');
-                    element?.scrollIntoView({behavior: 'smooth'});  
-                } }/>
-                <NavButton label={'Contact'} onClick={function (): void {
-                    const element = document.getElementById('contact');
-                    element?.scrollIntoView({behavior: 'smooth'});  
-                } }/>
+            <div className={`flex h-full w-1/2 justify-end mt-6`}>
+                { isMobView ? (
+                    // Desktop View 
+                    <div className='flex justify-end w-full h-full'>
+                        <NavButton label={'Home'} onClick={function (): void {
+                            const element = document.getElementById('home');
+                            element?.scrollIntoView({behavior: 'smooth'});  
+                        } }/>
+                        <NavButton label={'Projects'} onClick={function (): void {
+                            const element = document.getElementById('projects');
+                            element?.scrollIntoView({behavior: 'smooth'});  
+                        } }/>
+                        <NavButton label={'Contact'} onClick={function (): void {
+                            const element = document.getElementById('contact');
+                            element?.scrollIntoView({behavior: 'smooth'});  
+                        } }/>
+                    </div>
+                ) : (
+                    // Mobile View 
+                    <div className={`flex h-full w-full justify-end m-8`}>
+                        <Menu size={30} color="white" onClick={() => {setMenuOpen(true)}}/>
+                    </div>
+                )     
+            }    
             </div>
         </nav>
     )
