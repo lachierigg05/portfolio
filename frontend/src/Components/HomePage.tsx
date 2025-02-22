f41193dbe61b9ba7b0a5f3a6ea4e0a60459c05c3
Lachie Rigg <lachrigg@gmail.com>
2025-02-22 16:43:33 +1100

    Added navbar transitions, styling and animations

--- frontend/src/Components/HomePage.tsx
+++ frontend/src/Components/HomePage.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import NavBar from './NavBar';
+import StaticNav from './StaticNav';
 
 export const HomePage: React.FC = () => {
     return (
         <div id='home' className="h-screen w-full flex justify-center border-b border-t border-gray-800">
-            
+            <StaticNav/>
         </div>
     )
 }
--- frontend/src/Components/StaticNav.tsx
+++ frontend/src/Components/StaticNav.tsx
@@ -1,33 +1,36 @@
 import React, { useEffect, useState } from 'react'; 
 import NavButton from './NavButton';
 
-export const NavBar: React.FC = () => {
-    const [showNavbar, setShowNavBar] = useState(false);
-    
+const StaticNav: React.FC = () => {
+    const [inView, setInView] = useState(false);
+
     useEffect(() => {
+        setInView(true);
+        
         const handleScroll = () => {
             const offset = window.scrollY; 
-            if (offset > 75) {
-                setShowNavBar(true);
+            if (offset === 0) {
+                setInView(true); 
             } else {
-                setShowNavBar(false); 
+                setInView(false); 
             }
         }
 
-        window.addEventListener('scroll', handleScroll);
+        window.addEventListener('scroll', handleScroll); 
 
         return () => {
-            window.removeEventListener('scroll', handleScroll);
+            window.removeEventListener('scroll', handleScroll); 
         }
-    })
-    
+
+    }, []);
+
     
     return (
-        <nav className={`fixed flex flex-row justify-center items-center t-0 h-30 w-full bg-gray-950 shadow-2xl shadow-blue-900 transition-transform duration-500 ${showNavbar ? 'translate-y-0' : '-translate-y-100'}`}>
-            <div className="flex h-full w-1/2 ml-10 items-center mt-4">
+        <nav className={`fixed flex justify-start h-30 w-full bg-gray-950 shadow-blue-500 transition-opacity duration-800 ${inView ? 'opacity-100' : 'opacity-0'}`}>
+            <div className="flex h-full w-1/2 ml-10 items-center">
                 <h1 className={'text-white font-popsies text-4xl'}>Lachie Rigg</h1>
             </div>
-            <div className={`flex h-full w-1/2 justify-end mt-6`}>
+            <div className={`flex h-full w-1/2 justify-end`}>
                 <NavButton label={'Home'} onClick={function (): void {
                     const element = document.getElementById('home');
                     element?.scrollIntoView({behavior: 'smooth'});  
@@ -45,4 +48,4 @@ export const NavBar: React.FC = () => {
     )
 }
 
-export default NavBar;
+export default StaticNav; 
